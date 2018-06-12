aaa3addddfae83405ebf28b1f22ff971d9a1f080
Stefan Piazza <stefanpiazza@gmail.com>
2018-06-12 19:43:46 +0100

    UPDATE: router added to test state permanence

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,50 @@
 var styles = require('./App.scss');
 
 import React from 'react';
+import { render } from 'react-dom';
+import { Link, BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 import Button from './components/Button';
+
+class Home extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+        	<AppContext.Consumer>
+        	    {(context) => (
+        	        <div>
+        	        	<p>Home</p>
+        	            <Button text='Increase' onClick={context.inreaseCount}/>
+        	            <Button text='Decrease' onClick={context.decreaseCount}/>
+        	        </div>
+        	    )}
+        	</AppContext.Consumer>
+        )
+    }
+}
+
+class All extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+        	<AppContext.Consumer>
+        	    {(context) => (
+        	        <div>
+        	        	<p>All</p>
+        	            <Button text='Increase' onClick={context.inreaseCount}/>
+        	            <Button text='Decrease' onClick={context.decreaseCount}/>
+        	        </div>
+        	    )}
+        	</AppContext.Consumer>
+        )
+    }
+}
 
 const AppContext = React.createContext();
 
@@ -14,14 +56,25 @@ class AppProvider extends React.Component {
         this.state = {
             count: 0
         }
+
+        this.increaseCount = this.increaseCount.bind(this);
+        this.decreaseCount = this.decreaseCount.bind(this);
+    }
+
+    increaseCount() {
+    	this.setState({count: this.state.count += 1})
+    }
+
+    decreaseCount() {
+    	this.setState({count: this.state.count -= 1})
     }
 
     render() {
         return (
             <AppContext.Provider value={{
                 state: this.state,
-                inreaseCount: () => this.setState({count: this.state.count += 1}),
-                decreaseCount: () => this.setState({count: this.state.count -=1})
+                inreaseCount: this.increaseCount,
+                decreaseCount: this.decreaseCount,
             }}>
                 {this.props.children}
             </AppContext.Provider>
@@ -32,23 +85,55 @@ class AppProvider extends React.Component {
 class App extends React.Component {
     constructor(props) {
         super(props);
+        this.state = {
+            count: 0
+        }
+
+        this.increaseCount = this.increaseCount.bind(this);
+        this.decreaseCount = this.decreaseCount.bind(this);
     }
+
+    increaseCount() {
+    	this.setState({count: this.state.count += 1})
+    }
+
+    decreaseCount() {
+    	this.setState({count: this.state.count -= 1})
+    }
+
+    // constructor(props) {
+    //     super(props);
+    // }
 
     render() {
         return (
-            <AppProvider>
+        	<AppContext.Provider value={{
+        	    state: this.state,
+        	    inreaseCount: this.increaseCount,
+        	    decreaseCount: this.decreaseCount,
+        	}}>
+            {/*<AppProvider>*/}
                 <div className='app'>
-                    <AppContext.Consumer>
-                        {(context) => (
-                            <div>
-                                <p>Current Count: {context.state.count}</p>
-                                <Button text='Increase' onClick={context.inreaseCount}/>
-                                <Button text='Decrease' onClick={context.decreaseCount}/>
-                            </div>
-                        )}
-                    </AppContext.Consumer>
+                   	<AppContext.Consumer>
+                	    {(context) => (
+                	        <div>
+                	            <p>Current Count: {context.state.count}</p>
+                	        </div>
+                	    )}
+                	</AppContext.Consumer>
+                	<Router>
+                		<React.Fragment>
+	    	            	<Link to='/'>Home</Link>
+	                		<Link to='/all'>All</Link>
+	                		<Switch>
+	                		    <Route exact path='/' component={Home}/>
+	                		    <Route exact path='/all' component={All}/>
+	                		</Switch>
+                		</React.Fragment>
+	                </Router>
                 </div>
-            </AppProvider>
+            {/*</AppProvider>*/}
+            </AppContext.Provider>
         )
     }
 }
